8f9a55ff568708d5829c2b08e6308d1970ddf05f
ailun001 <43659626+ailun001@users.noreply.github.com>
2019-05-26 18:12:32 +1200

    Add files via upload

--- Takashi/k.tree/category/post.php
+++ Takashi/k.tree/category/post.php
@@ -3,8 +3,8 @@
     
     <?php
 	$to = "ailun001kkjoker@gmail.com";
-	$subject = $_POST["order"];
-	$message = $_POST["address"];
+	$subject = "Order: ".$_POST["order"];
+	$message = "Address; ".$_POST["address"];
 	$from = $_POST["name"];
 	$headers = "From: $from";
 	mail($to,$subject,$message,$headers);
@@ -13,8 +13,22 @@
     echo '<br>';
     echo '<p>Thanks for you purchase</p>';
     echo '<br><br>';
-    echo '<p>Contact:tree@mail.com</p>';
-    
+    echo '<h2>Mt Eden Branch:</h2>
+            <h3>
+                445 Mount Eden Road, Mount Eden, Auckland</br>
+				Contact: Stephen Beck, 09 789 4455</br></br>
+            </h3>
+			<h2>East Tamaki Branch:</h2>
+            <h3>
+                29 Kerwyn Ave, East Tamaki, Auckland 2013<br>
+				Contact: Amy Anderson, 09 789 4444</br></br>
+            </h3>
+			<h2>Albany Branch:</h2>
+            <h3>
+                67 Kell Dr, Albany, Auckland 0632</br>
+				Contact: Chris  Wilson, 09 789 1458</br></br>
+			</br></br>
+            </h3>';
     ?>
     
 </div>
--- Takashi/k.tree/tool/description.php
+++ Takashi/k.tree/tool/description.php
@@ -9,8 +9,22 @@
     echo '<br>';
     echo '<p>Thanks for you purchase</p>';
     echo '<br>';echo '<br>';
-    echo '<p>Contact:tree@mail.com</p>';
-	echo 'Address: ??, Auckland';
+	echo '		<h2>Mt Eden Branch:</h2>
+            <h3>
+                445 Mount Eden Road, Mount Eden, Auckland</br>
+				Contact: Stephen Beck, 09 789 4455</br></br>
+            </h3>
+			<h2>East Tamaki Branch:</h2>
+            <h3>
+                29 Kerwyn Ave, East Tamaki, Auckland 2013<br>
+				Contact: Amy Anderson, 09 789 4444</br></br>
+            </h3>
+			<h2>Albany Branch:</h2>
+            <h3>
+                67 Kell Dr, Albany, Auckland 0632</br>
+				Contact: Chris  Wilson, 09 789 1458</br></br>
+			</br></br>
+            </h3>';
     }elseif ($_POST['choose']=="delivery" ) {
         echo '<h1>Please Enter You Detial</h1>';
 		echo '<form method="post" action="post.php">';
