d00a5fab3138c8ad6d3433d02ae2ace6015b2248
ailun001 <43659626+ailun001@users.noreply.github.com>
2019-05-26 18:05:08 +1200

    Add files via upload

--- Takashi/k.tree/tool.php
+++ Takashi/k.tree/tool.php
@@ -5,27 +5,27 @@
         <div class="shop-items">
             <div class="shop-item">
                 <span class="shop-item-title">Square shovel</span>
-                <a href="./tool/Squareshovel.html"><img src="./..Images/square_shovel.png" alt="Square shovel" width="300" height="300"></a>
+                <a href="./tool/Squareshovel.php"><img src="./../Images/square_shovel.png" alt="Square shovel" width="300" height="300"></a>
             </div>
             <div class="shop-item">
                 <span class="shop-item-title">Round shovel</span>
-                <a href="./tool/RoundMouthShovel.html"><img src="./..Images/roundshovel.png" alt="Round shovel" width="300" height="300"></a>
+                <a href="./tool/RoundMouthShovel.php"><img src="./../Images/roundshovel.png" alt="Round shovel" width="300" height="300"></a>
             </div>
             <div class="shop-item">
                 <span class="shop-item-title">Snail bait</span>
-                <a href="./tool/SnailBait.html"><img src="./..Images/snail_bait.png" alt="Snail bait" width="300" height="300"></a>
+                <a href="./tool/SnailBait.php"><img src="./../Images/snail_bait.png" alt="Snail bait" width="300" height="300"></a>
             </div>
             <div class="shop-item">
                 <span class="shop-item-title">Rose and flower fertilizer</span>
-                <a href="./tool/Roseandflowerfertilizer.html"><img src="./..Images/rose_flower_fertilizer.png" alt="Rose and flower fertilizer" width="300" height="300"></a>
+                <a href="./tool/Roseandflowerfertilizer.php"><img src="./../Images/rose_flower_fertilizer.png" alt="Rose and flower fertilizer" width="300" height="300"></a>
             </div>
             <div class="shop-item">
                 <span class="shop-item-title">Citrus fertilizer</span>
-                <a href="./tool/Citrusfertilizer.html"><img src="./..Images/citrus_fertilizer.png" alt="Citrus fertilizer" width="300" height="300"></a>
+                <a href="./tool/Citrusfertilizer.php"><img src="./../Images/citrus_fertilizer.png" alt="Citrus fertilizer" width="300" height="300"></a>
             </div>
             <div class="shop-item">
                 <span class="shop-item-title">Potting mix</span>
-                <a href="./tool/Premiumpottingmix.html"><img src="./..Images/Potting_mix.png" alt="Potting mix" width="300" height="300"></a>
+                <a href="./tool/Premiumpottingmix.php"><img src="./../Images/Potting_mix.png" alt="Potting mix" width="300" height="300"></a>
             </div>
  
         </div>
